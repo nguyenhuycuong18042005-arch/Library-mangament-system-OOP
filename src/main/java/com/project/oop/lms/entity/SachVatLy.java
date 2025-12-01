b36835c63ec277bc88cbefca0d9e1eee1a510ac3
Phat <miku6122006@gmail.com>
2025-12-02 03:48:43 +0700

    Update SachVatLy.java

--- src/main/java/com/project/oop/lms/entity/SachVatLy.java
+++ src/main/java/com/project/oop/lms/entity/SachVatLy.java
@@ -1,5 +1,68 @@
 package com.project.oop.lms.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "sach_vat_ly")
 public class SachVatLy {
-    // TODO: Thêm các thuộc tính, getter, setter, constructor
+
+    public enum TinhTrang {
+        SAN_SANG,
+        DANG_MUON,
+        HONG
+    }
+
+    @Id
+    @Column(name = "ma_vach", nullable = false)
+    private String maVach;
+
+    @Column(name = "isbn", nullable = false)
+    private String isbn;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tinh_trang", nullable = false)
+    private TinhTrang tinhTrang;
+
+    public SachVatLy() {
+        // JPA bắt buộc phải có constructor rỗng
+    }
+
+    public SachVatLy(String maVach, String isbn) {
+        this.maVach = maVach;
+        this.isbn = isbn;
+        this.tinhTrang = TinhTrang.SAN_SANG;  // mặc định như bản của bạn
+    }
+
+    public String getMaVach() {
+        return maVach;
+    }
+
+    public void setMaVach(String maVach) {
+        this.maVach = maVach;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public TinhTrang getTinhTrang() {
+        return tinhTrang;
+    }
+
+    public void setTinhTrang(TinhTrang tinhTrang) {
+        this.tinhTrang = tinhTrang;
+    }
+
+    @Override
+    public String toString() {
+        return "SachVatLy{" +
+                "maVach='" + maVach + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", tinhTrang=" + tinhTrang +
+                '}';
+    }
 }
