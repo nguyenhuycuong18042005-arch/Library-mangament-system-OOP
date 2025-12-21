d447c615e8c175eff0404708b2090c24650e15f0
Phat <miku6122006@gmail.com>
2025-12-21 14:15:22 +0700

    Update SachVatLy.java

--- src/main/java/com/project/oop/lms/entity/SachVatLy.java
+++ src/main/java/com/project/oop/lms/entity/SachVatLy.java
@@ -1,5 +1,61 @@
 package com.project.oop.lms.entity;
 
 public class SachVatLy {
+
+    public enum TinhTrang {
+        SAN_SANG,
+        DANG_MUON,
+        HONG
+    }
+
+    private String maVach;
+    private String isbn;
+    private TinhTrang tinhTrang;
+
+    public SachVatLy() {
+    }
+
+    public SachVatLy(String maVach, String isbn) {
+        this.maVach = maVach;
+        this.isbn = isbn;
+        this.tinhTrang = TinhTrang.SAN_SANG;
+    }
+
+    public String getMaVach() {
+        return maVach;
+    }
+
+    public void setMaVach(String maVach) {
+        this.maVach = maVach;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public TinhTrang getTinhTrang() {
+        return tinhTrang;
+    }
+
+    public void setTinhTrang(TinhTrang tinhTrang) {
+        this.tinhTrang = tinhTrang;
+    }
+
+    @Override
+    public String toString() {
+        return "SachVatLy{" +
+                "maVach='" + maVach + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", tinhTrang=" + tinhTrang +
+                '}';
+    }
+}
+package com.project.oop.lms.entity;
+
+public class SachVatLy {
     // TODO: Thêm các thuộc tính, getter, setter, constructor
 }
