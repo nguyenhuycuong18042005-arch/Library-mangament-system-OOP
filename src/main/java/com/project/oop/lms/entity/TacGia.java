611ac1638b6d1d402c142e2be9a0edadf3349514
Phat <miku6122006@gmail.com>
2025-12-21 14:15:39 +0700

    Add properties and methods to TacGia class

--- src/main/java/com/project/oop/lms/entity/TacGia.java
+++ src/main/java/com/project/oop/lms/entity/TacGia.java
@@ -1,5 +1,46 @@
 package com.project.oop.lms.entity;
 
 public class TacGia {
-    // TODO: Thêm các thuộc tính, getter, setter, constructor
+
+    private String maTG;
+    private String hoTen;
+    private String quocTich;
+
+    public TacGia() {
+    }
+
+    public TacGia(String maTG, String hoTen, String quocTich) {
+        this.maTG = maTG;
+        this.hoTen = hoTen;
+        this.quocTich = quocTich;
+    }
+
+    public String getMaTG() {
+        return maTG;
+    }
+
+    public void setMaTG(String maTG) {
+        this.maTG = maTG;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getQuocTich() {
+        return quocTich;
+    }
+
+    public void setQuocTich(String quocTich) {
+        this.quocTich = quocTich;
+    }
+
+    @Override
+    public String toString() {
+        return "TG[" + maTG + "]: " + hoTen + " (" + quocTich + ")";
+    }
 }
